5f3683a6850a45e5e56c70041c38a04edcd41fc5
Sailor20 <201688516@qq.com>
2017-05-02 17:38:50 +0800

    完善文档信息

--- api-server/doc/apiDoc/im/tokenDoc.js
+++ api-server/doc/apiDoc/im/tokenDoc.js
@@ -3,7 +3,7 @@
  */
 
 /**
- * @apiIgnore {get} /v1/im/tokens 获取签名信息
+ * @api {get} /v1/im/tokens 获取签名信息
  * @apiDescription 签名使用过程中不排除失效的可能性，前端最好做一下失效处理，失效时，从接口重新拿取新的签名信息
  * @apiVersion 1.0.0
  * @apiName getSig
@@ -28,20 +28,24 @@
  * @apiSuccessExample 正常响应：
  *    HTTP/1.1 200 Success
  *    {
- *      "sdkAppID": "1400022000",
- *      "appIDAt3rd": "1400022000",
- *      "accountType": "9000",
- *      "identifier": "58256153622c083e8c1537ea",
- *      "identifierNick": "张老师",
- *      "userSig": "eJxtzE1rgzAcgPHvkuvGyNKkaGGnOIp0ljrj3i4hNWn3R6MhZr
- *        S27LtPbLfTrs8PnjMST8Wdcg60VEHOvEYLdE8xxoTgmKHbi1dV99UGGQZnRo-Z
- *        HF8FtGkD7MD4sVcKLFyh17Wctv-9Algz9YiMFOHfmzk68EaqXZh*0Zz*UQ-7sWSPJU-zJ
- *        EtvdFwMtsbcPrf4bb09hU2zPa42VAu-aijYmSDDxzLP072hy1cWDiJZl8Fj5TrGP991kiV
- *        VWXD3InhNDqwTWdz0D*j7B-RpU_o_"
+ *      "code": 0,
+ *      "msg": "成功",
+ *      "data": {
+ *        "sdkAppID": "1400022000",
+ *        "appIDAt3rd": "1400022000",
+ *        "accountType": "9000",
+ *        "identifier": "58256153622c083e8c1537ea",
+ *        "identifierNick": "张老师",
+ *        "userSig": "eJxtzE1rgzAcgPHvkuvGyNKkaGGnOIp0ljrj3i4hNWn3R6MhZr
+ *          S27LtPbLfTrs8PnjMST8Wdcg60VEHOvEYLdE8xxoTgmKHbi1dV99UGGQZnRo-Z
+ *          HF8FtGkD7MD4sVcKLFyh17Wctv-9Algz9YiMFOHfmzk68EaqXZh*0Zz*UQ-7sWSPJU-zJ
+ *          EtvdFwMtsbcPrf4bb09hU2zPa42VAu-aijYmSDDxzLP072hy1cWDiJZl8Fj5TrGP991kiV
+ *          VWXD3InhNDqwTWdz0D*j7B-RpU_o_"
+ *      }
  *    }
  *
  * @apiErrorExample 错误响应：
- *    HTTP/1.1 400
+ *    HTTP/1.1 200
  *    {
  *       "code": 500,
  *       "msg": "系统错误"
--- api-server/doc/apiDoc/userDoc.js
+++ api-server/doc/apiDoc/userDoc.js
@@ -17,7 +17,8 @@
  * @apiParam {String} password 密码的md5密文
  *
  * @apiExample {bash} 请求样例：
- *    curl -d 'username=test&password=e10adc3949ba59abbe56e057f20f883e' https://www.dlkj.com/v1/test
+ *    curl -d 'username=test&password=e10adc3949ba59abbe56e057f20f883e'
+ *    https://www.dlkj.com/v1/users/register
  *
  * @apiSuccess {Number} code 代码
  * @apiSuccess {String} msg 提示内容
@@ -26,16 +27,16 @@
  * @apiSuccessExample 正常响应：
  *    HTTP/1.1 200 OK
  *    {
-   *       "code": 0,
-   *       "msg": "成功"
-   *    }
+ *       "code": 0,
+ *       "msg": "成功"
+ *    }
  *
  * @apiErrorExample 错误响应：
- *    HTTP/1.1 500
+ *    HTTP/1.1 200
  *    {
-   *       "code": 500,
-   *       "msg": "系统错误",
-   *    }
+ *       "code": 1100,
+ *       "msg": "用户不存在",
+ *    }
  */
 
 
@@ -49,27 +50,32 @@
  * @apiParam {String} password 密码的md5密文
  *
  * @apiExample {bash} 请求样例：
- *    curl -d 'username=test&password=e10adc3949ba59abbe56e057f20f883e' https://www.dlkj.com/v1/test
+ *    curl -d 'username=test&password=e10adc3949ba59abbe56e057f20f883e'
+ *    https://www.dlkj.com/v1/users/login
  *
  * @apiSuccess {Number} code 代码
  * @apiSuccess {String} msg 提示内容
+ * @apiSuccess {Object} data 返回用户信息
+ * @apiSuccess {String} data.id 用户id
+ * @apiSuccess {String} data.username 用户名
+ * @apiSuccess {String} data.token 用户token
  *
  *
  * @apiSuccessExample 正常响应：
  *    HTTP/1.1 200 OK
  *    {
-   *       "code": 0,
-   *       "msg": "http 测试接口",
-   *       "data": {
-   *         "id": "e10adc3949ba59abbe56e057",
-   *         "username": "test",
-   *       }
-   *    }
+ *       "code": 0,
+ *       "msg": "http 测试接口",
+ *       "data": {
+ *         "id": "e10adc3949ba59abbe56e057",
+ *         "username": "test",
+ *       }
+ *    }
  *
  * @apiErrorExample 错误响应：
- *    HTTP/1.1 500
+ *    HTTP/1.1 200
  *    {
-   *       "code": 500,
-   *       "msg": "系统错误",
-   *    }
+ *       "code": 1102,
+ *       "msg": "用户名或密码不正确",
+ *    }
  */
